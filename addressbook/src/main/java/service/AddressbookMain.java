827175fdfdd965e1aa0a6748b4d078ca53d99373
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-07-09 07:59:22 +0530

    added : display welcome message

---
+++ addressbook/src/main/java/service/AddressbookMain.java
@@ -0,0 +1,17 @@
+/**************************************************************
+ * Purpose : Service class for address book problem. 
+ * @author Hrishikesh Ugavekar
+ * @Version 1.0
+ * @since 9-07-2021
+ *
+ ************************************************************/
+package service;
+public class AddressbookMain {
+	
+	/**
+	 * Display welcome message.
+	 */
+	public void displayWelcome() {
+		System.out.println("Welcome to address book");
+	}
+}
